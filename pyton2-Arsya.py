706b50a1edd9b280d8e95951731c0cbb6ffeea23
syaarifqi123 <111481181+syaarifqi123@users.noreply.github.com>
2022-08-18 00:01:03 -0700

    Add files via upload

---
+++ pyton2-Arsya.py
@@ -0,0 +1,6 @@
+nama_depan="Arsya"
+nama_belakang="Rifqi"
+nama_lengkap="M.Arsya Rifqi Maulana Anwar"
+print("nama depan saya adalah",nama_depan)
+print("nama belakang saya adalah",nama_belakang)
+print("nama lengkap saya adalah",nama_depan,nama_belakang)
