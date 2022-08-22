413a7e6274fc573b154bf6f275e0a166a98ab635
syaarifqi123 <111481181+syaarifqi123@users.noreply.github.com>
2022-08-21 23:33:33 -0700

    Add files via upload

---
+++ python4-latihan.py
@@ -0,0 +1,34 @@
+print("============")
+print("rumus bangin datar")
+print("============/n")
+
+print("1. persegi/n)
+
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi
+print("luas persegi adalah", luas_p)
+
+print("/n2 persegi panjang")
+
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_s = 1/2 * tinggi / 2
+
+print("luas segitiga adalah: ", luas_s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
