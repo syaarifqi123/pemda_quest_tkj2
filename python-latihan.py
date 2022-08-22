afda21d78b00277342c82a7b62e9fc10bcc7734a
syaarifqi123 <111481181+syaarifqi123@users.noreply.github.com>
2022-08-21 22:11:59 -0700

    Add files via upload

---
+++ python-latihan.py
@@ -0,0 +1,16 @@
+print("hallo semuanya")
+print("perkenalkan")
+nama_depan =input("nama depan saya")
+nama_belakang =input("nama belakang saya")
+
+a =int(input("masukan nilai A"))
+b =float(input("masukan nilai B"))
+C = A = b
+
+print("A =",a)
+print("B =",b)
+print(a,"+",b,"=", c)
+
+x = int(input("masukan nilai celcius"))
+y = (9/5 * x) + 32
+print(x, "celcius ="),y, "fahreheit"
