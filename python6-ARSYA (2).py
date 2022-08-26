b41c40d5bbbf50f48b8d0a1eeda896dcb68c9d42
syaarifqi123 <111481181+syaarifqi123@users.noreply.github.com>
2022-08-26 08:54:35 +0700

    Add files via upload

---
+++ python6-ARSYA (2).py
@@ -0,0 +1,34 @@
+print("============")
+print("bangun ruang")
+print("============")
+
+print("\n1,persegi panjang")
+
+panjang = int(input("masukan nilai panjang: "))
+lebar = int(input("masukan nilai lebar: "))
+luas_pp = panjang * lebar 
+print("luas persegi panjang adalah:", luas_pp)
+
+print("\n2,segitiga")
+
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_s = alas * tinggi / 2
+print("luas segitiga adlah: ", luas_s)
+
+print("\n3,lingkaran")
+
+r =int(input("masukan jari jari: "))
+keliling = 2 * 3,14 * r
+luas = 3,14 * r * r
+print("keliling =" ,keliling)
+print("luas =" ,luas)
+
+print("\n4,jajargenjang")
+
+alas = float(input("masukan nilai alas: "))
+tinggi = float(input("masukan nilai tinggi: "))
+luas_jg = alas * tinggi
+print("luas jajargenjang adalah", luas_jg)
+
+
