2b50cb941bd1b1b9ca367d787fbe82b3ccf83ab8
syaarifqi123 <111481181+syaarifqi123@users.noreply.github.com>
2023-02-13 09:56:02 +0700

    Add files via upload

---
+++ latihan_2.py
@@ -0,0 +1,18 @@
+'''
+buatlah program menghitung luas bangun datar menggunakan fungsi.
+- segitiga
+- persegi panjang
+'''
+
+def luas_s(alas, tinggi):
+    luas =1/2 * alas * tinggi
+    print("luas segitiga adalah", luas)
+
+luas_s(5, 1)
+
+def luas_pp(panjang, lebar):
+    luas = panjang * lebar
+    print("luas persegi panjang", luas)
+
+luas_pp(7,3)
+
---
+++ latihan_fungsi.py
@@ -0,0 +1,41 @@
+
+
+"def nama_funsi():"
+
+def salam():
+    print("assalamu'alaikum !! akhi")
+
+salam()
+salam()
+salam()
+
+#buatlah fungsi fersi kalian
+
+def makanan():
+    print("ketoprak")
+
+makanan()
+makanan()
+makanan()
+
+#fungsi dengan parameter
+#def nama_fungsi(p1, p2)
+
+def say_hi(nama):
+    print("hallo perkenalkan nama saya", nama)
+
+say_hi("jawir")
+say_hi("werrr")
+
+#buatlah fungsi dengan 3 parameter
+
+def nama_hewan(hewan1, hewan2, hewan3):
+    print("hewan adalah:", hewan1)
+    print("hewan adalah:", hewan2)
+    print("hewan adalah:", hewan3)
+
+nama_hewan("gajah", "burung", "ular")
+
+
+
+
